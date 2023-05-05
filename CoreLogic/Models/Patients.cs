6f05cba70333e51d1eb24073988a3fed52281fcc
Gabriel Villarreal Ponce <gabriel.villarreal.ponce@gmail.com>
2023-05-04 23:29:35 -0400

    Addition of the directory Models and inside it Patients.cs, all of this in the directory CorLogic

---
+++ CoreLogic/Models/Patients.cs
@@ -0,0 +1,10 @@
+namespace upbThree.CoreLogic.Models;
+
+public class Patients
+{
+    public string BG {get; set;}
+    public string LN {get; set;}
+    public string Nume {get; set;}
+    public int ID {get; set;}
+    public int YearsOnEarth {get; set;}
+}
